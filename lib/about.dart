c0dc58612c33fcc23f4df42410637555d745a1a0
zonble <zonble@gmail.com>
2019-08-25 00:00:24 +0800

    Adds co-organizers.

--- lib/about.dart
+++ lib/about.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iplayground19/api/src/sponsor.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'data_bloc.dart';
 
@@ -37,28 +38,19 @@ class _AboutPageState extends State<AboutPage> {
 
   @override
   Widget build(BuildContext context) {
-    final venueWidgets = <Widget>[
-      AboutSectionTitle(text: 'Venue 場地'),
-      Text('國立臺灣大學博雅教學館'),
-    ];
-    final venueSection =
-        SliverList(delegate: SliverChildListDelegate(venueWidgets));
-
-    final aboutWidgets = <Widget>[
-      AboutSectionTitle(text: 'About 關於我們'),
-      Text(
-          '2017年9月，一群到東京參加 iOSDC 的工程師們，在看到國外蓬勃活躍的程式力，熱血自此被點燃，決心舉辦一場兼具廣深度又有趣的 iOS 研討會。'),
-      SizedBox(height: 5),
-      Text('2018年10月，有實戰技巧、初心者攻略、hard core 議題以及各式八卦政治學的 iPlaygrouond 華麗登場。'),
-      SizedBox(height: 5),
-      Text('2019年，iPlayground 誠摯召喚各位鍵盤好手一起來燃燒熱血，讓議程更多元、更有料！')
-    ];
-    final aboutSection =
-        SliverList(delegate: SliverChildListDelegate(aboutWidgets));
-
+    final logo = SliverToBoxAdapter(child: Image.asset('images/logo.png'));
+    final venueSection = makeVenue();
+    final aboutSection = makeAboutUs();
     final sponsorTitle = SliverList(
         delegate:
             SliverChildListDelegate([AboutSectionTitle(text: 'Sponsors 贊助')]));
+    final coTitle = SliverList(
+        delegate: SliverChildListDelegate(
+            [AboutSectionTitle(text: 'Co-organizers 合作夥伴')]));
+    final coGrid = makeCoOrganizersGrid();
+
+    // --
+
     DataBloc bloc = BlocProvider.of(context);
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(middle: Text('關於')),
@@ -70,12 +62,98 @@ class _AboutPageState extends State<AboutPage> {
             bloc: bloc,
             builder: (context, state) {
               var slivers = <Widget>[];
-              slivers.addAll([venueSection, aboutSection, sponsorTitle]);
+              slivers.addAll([
+                logo,
+                venueSection,
+                aboutSection,
+                sponsorTitle,
+              ]);
               slivers.addAll(makeSponsorGrid(state));
+              slivers.addAll([
+                coTitle,
+                coGrid,
+                SliverToBoxAdapter(child: Container(height: 60.0))
+              ]);
               return CustomScrollView(slivers: slivers);
             },
           ),
         ))));
+  }
+
+  SliverList makeVenue() {
+    final venueWidgets = <Widget>[
+      AboutSectionTitle(text: 'Venue 場地'),
+      Text('國立臺灣大學博雅教學館'),
+    ];
+    final venueSection =
+        SliverList(delegate: SliverChildListDelegate(venueWidgets));
+    return venueSection;
+  }
+
+  SliverList makeAboutUs() {
+    final aboutWidgets = <Widget>[
+      AboutSectionTitle(text: 'About 關於我們'),
+      Text(
+          '2017年9月，一群到東京參加 iOSDC 的工程師們，在看到國外蓬勃活躍的程式力，熱血自此被點燃，決心舉辦一場兼具廣深度又有趣的 iOS 研討會。'),
+      SizedBox(height: 5),
+      Text('2018年10月，有實戰技巧、初心者攻略、hard core 議題以及各式八卦政治學的 iPlaygrouond 華麗登場。'),
+      SizedBox(height: 5),
+      Text('2019年，iPlayground 誠摯召喚各位鍵盤好手一起來燃燒熱血，讓議程更多元、更有料！')
+    ];
+    final aboutSection =
+        SliverList(delegate: SliverChildListDelegate(aboutWidgets));
+    return aboutSection;
+  }
+
+  makeCoOrganizersGrid() {
+    final List<List<String>> data = [
+      [
+        'images/co_aatp.png',
+        'https://aatp.com.tw',
+      ],
+      [
+        'images/co_cocoaheads_tw.png',
+        'https://www.facebook.com/groups/cocoaheads.taipei/'
+      ],
+      [
+        'images/co_ios_taipei.png',
+        'https://www.facebook.com/groups/ios.taipei/',
+      ],
+      [
+        'images/co_swift_taipei.png',
+        'https://www.meetup.com/Swift-Taipei-User-Group/',
+      ],
+      [
+        'images/co_app_girls.png',
+        'https://www.facebook.com/groups/1260405513988915/',
+      ],
+    ];
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        final item = data[index];
+        return LayoutBuilder(builder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  onTap: () => launch(item[1]),
+                  child: Container(),
+                ),
+              ),
+              width: constraints.maxWidth,
+              height: constraints.maxHeight,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage(item[0]))),
+            ),
+          );
+        });
+      }, childCount: data.length),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200.0,
+      ),
+    );
   }
 }
 
