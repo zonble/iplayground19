f22e19141d69412a328865e704e488755744b69b
zonble <zonble@gmail.com>
2019-08-25 01:30:26 +0800

    Adds images for staff.

--- lib/about.dart
+++ lib/about.dart
@@ -49,6 +49,11 @@ class _AboutPageState extends State<AboutPage> {
             [AboutSectionTitle(text: 'Co-organizers 合作夥伴')]));
     final coGrid = makeCoOrganizersGrid();
 
+    final staffTitle = SliverList(
+        delegate:
+            SliverChildListDelegate([AboutSectionTitle(text: 'Staffs 工作人員')]));
+    final staffGrid = makeStaffGrid();
+
     // --
 
     DataBloc bloc = BlocProvider.of(context);
@@ -62,16 +67,13 @@ class _AboutPageState extends State<AboutPage> {
             bloc: bloc,
             builder: (context, state) {
               var slivers = <Widget>[];
-              slivers.addAll([
-                logo,
-                venueSection,
-                aboutSection,
-                sponsorTitle,
-              ]);
+              slivers.addAll([logo, venueSection, aboutSection, sponsorTitle]);
               slivers.addAll(makeSponsorGrid(state));
               slivers.addAll([
                 coTitle,
                 coGrid,
+                staffTitle,
+                staffGrid,
                 SliverToBoxAdapter(child: Container(height: 60.0))
               ]);
               return CustomScrollView(slivers: slivers);
@@ -105,6 +107,282 @@ class _AboutPageState extends State<AboutPage> {
     return aboutSection;
   }
 
+  makeStaffGrid() {
+    final List<List<String>> data = [
+      [
+        'Hokila',
+        'images/s_hokila.jpg',
+        'Father/ Trello Lover',
+        'https://twitter.com/hokilaJ',
+      ],
+      [
+        'Dada',
+        'images/s_dada.jpg',
+        'iOS Developer @ KKBOX',
+        'https://twitter.com/nalydadad',
+      ],
+      [
+        'Superbil',
+        'images/s_superbil.png',
+        'Software Freelancer',
+        'https://twitter.com/superbil',
+      ],
+      [
+        '大軍',
+        'images/s_dj.jpg',
+        '程式、平面、動態設計都很有興趣，喜歡交朋友歡迎認識。',
+        'https://www.facebook.com/profile.php?id=100000194796912',
+      ],
+      [
+        '13 一三',
+        'images/s_13.jpg',
+        'I write cool apps for living.',
+        'https://twitter.com/ethanhuang13',
+      ],
+      [
+        '陳涵宇',
+        'images/s_hy.png',
+        '我後面有一隻毛毛蟲。',
+        'https://www.facebook.com/hanyu.chen.518',
+      ],
+      [
+        'Hao Lee',
+        'images/s_haolee.jpg',
+        'macOS Developer',
+        'https://twitter.com/twhaolee',
+      ],
+      [
+        'Luke Wu',
+        'images/s_lukewu.jpg',
+        'iOS Instructor at AppWorks School',
+        'https://www.facebook.com/mvp0627',
+      ],
+      [
+        'Will Chen',
+        'images/s_willchen.jpeg',
+        'iOS Developer',
+        'https://twitter.com/willchen00'
+      ],
+      [
+        'Toby Hsu',
+        'images/s_tobyhsu.jpg',
+        'tvOS Dev @ CATCHPLAY',
+        'https://twitter.com/HsuToby',
+      ],
+      [
+        'BigRoot',
+        'images/s_bigroot.jpeg',
+        'KKBOX iOS Developer',
+        'https://twitter.com/BigRootHsu',
+      ],
+      [
+        'Chung',
+        'images/s_chung.png',
+        'iOS Evangelist / Consultant / Trainer /Developer',
+        'https://twitter.com/ChungPlusDev'
+      ],
+      [
+        'Tank',
+        'images/s_tank.jpg',
+        'iOS Developer at KKday',
+        'https://twitter.com/tank1005',
+      ],
+      [
+        '鄭雅方',
+        'images/s_yf.png',
+        'APP Girls 創辦人',
+        'https://www.facebook.com/groups/1260405513988915/',
+      ],
+      [
+        'Steve Sun',
+        'images/s_stevensun.jpg',
+        'iOS Developer @ Hootsuite',
+        'https://fb.me/steve.sun.125'
+      ],
+      [
+        'MarkFly',
+        'images/s_markfly.png',
+        'iOS developer learning Android',
+        'https://www.facebook.com/mark33699',
+      ],
+      [
+        'JimmyLiao',
+        'images/s_jimmyliao.jpeg',
+        'Jimmyliao',
+        'https://twitter.com/jimmyliao',
+      ],
+      [
+        'Dan',
+        'images/s_dan.jpg',
+        'iOS Developer @Readmoo, Monster Hunter, Pokémon Master.',
+        'https://twitter.com/phy1988',
+      ],
+      [
+        'Mike',
+        'images/s_mike.jpeg',
+        'Rookie iOS Developer',
+        'https://www.facebook.com/mikechouo',
+      ],
+      [
+        'Bob Chang',
+        'images/s_bobchang.jpg',
+        'iOS dev chicken',
+        'https://twitter.com/bob910078',
+      ],
+      [
+        'Joe Chen',
+        'images/s_joechen.jpg',
+        '我程式不會動，我不知道為什麼；我程式會動，我不知道為什麼',
+        'https://twitter.com/joe_trash_talk',
+      ],
+      [
+        'Kennedy',
+        'images/s_kennedy.png',
+        'iOS Developer ',
+        null,
+      ],
+      [
+        'Johnny Sung',
+        'images/s_johnnysung.jpg',
+        'iOS / Android Developer',
+        'https://fb.me/j796160836',
+      ],
+      [
+        'OOBE',
+        'images/s_oobe.png',
+        'Producer',
+        'https://twitter.com/OOBE',
+      ],
+      [
+        'Yoda',
+        'images/s_yoda.jpg',
+        'Jedi / Designer / Developer',
+        'https://www.facebook.com/YongSaingWang'
+      ],
+      [
+        'Gerry',
+        'images/s_gerry.png',
+        '佛心軟體工程師',
+        'https://twitter.com/gerry73740659',
+      ],
+      [
+        'TaiHsin',
+        'images/s_taihsin.jpg',
+        'iOS Developer @ KKBOX',
+        'https://www.facebook.com/peter.lee.752487'
+      ],
+      [
+        'Pofat',
+        'images/s_pofat.jpg',
+        '本鵝用翅膀寫 code',
+        'https://twitter.com/PofatTseng',
+      ],
+      [
+        'Cindy',
+        'images/s_cindy.jpeg',
+        'iOS Developer @H2Sync',
+        'https://www.facebook.com/hsin.chen.10',
+      ],
+      [
+        'Jeffrey Wang',
+        'images/s_jefferywang.jpg',
+        'Tech Lover / PM',
+        'https://www.facebook.com/jeffrey.wang.505',
+      ],
+      [
+        'Allen Lai',
+        'images/s_allenlai.jpg',
+        'iOS Developer',
+        'https://twitter.com/AllenEzailLai',
+      ],
+      [
+        '啊嘶',
+        'images/s_as.jpg',
+        '程式碼行數減少，體重卻默默上升',
+        'https://www.facebook.com/profile.php?id=100000061272837',
+      ],
+      [
+        'Mack Liu',
+        'images/s_mackliu.jpg',
+        'iOS / .NET Developer',
+        'https://www.facebook.com/bazhe1106'
+      ],
+      [
+        'Lim Yang',
+        'images/s_limyang.jpg',
+        'system engineer at Thinking Software',
+        'https://www.facebook.com/arawn.yang',
+      ],
+      [
+        'Alice',
+        'images/s_alice.jpg',
+        'iOS developer @H2sync',
+        'https://www.facebook.com/profile.php?id=100002422822162',
+      ],
+      [
+        'Annie Li',
+        'images/s_annieli.jpg',
+        'iOS Developer @ KKBOX',
+        'https://www.facebook.com/profile.php?id=1824210769',
+      ],
+      [
+        'Roger',
+        'images/s_roger.jpg',
+        'iOS Developer',
+        'https://twitter.com/roger_fanfan',
+      ],
+      [
+        'Crystal',
+        'images/s_crystal.jpg',
+        'iOS Developer',
+        'https://www.facebook.com/liu.crystal.9',
+      ]
+    ];
+
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate((context, index) {
+        final item = data[index];
+        return LayoutBuilder(builder: (context, constraints) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ClipOval(
+                child: Container(
+                  width: constraints.maxWidth,
+                  height: constraints.maxWidth,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage(item[1]))),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {
+                        var link = item[3];
+                        if (link != null) {
+                          launch(link);
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(item[0], style: TextStyle(fontSize: 22.0)),
+              SizedBox(height: 4),
+              Text(
+                item[2],
+                textAlign: TextAlign.center,
+              ),
+            ],
+          );
+        });
+      }, childCount: data.length),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 0.5,
+          crossAxisSpacing: 10.0),
+    );
+  }
+
   makeCoOrganizersGrid() {
     final List<List<String>> data = [
       [
@@ -132,26 +410,24 @@ class _AboutPageState extends State<AboutPage> {
       delegate: SliverChildBuilderDelegate((context, index) {
         final item = data[index];
         return LayoutBuilder(builder: (context, constraints) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () => launch(item[1]),
-                  child: Container(),
-                ),
+          return Container(
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () => launch(item[1]),
+                child: Container(),
               ),
-              width: constraints.maxWidth,
-              height: constraints.maxHeight,
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(item[0]))),
             ),
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(item[0]))),
           );
         });
       }, childCount: data.length),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200.0,
+        crossAxisSpacing: 10.0,
       ),
     );
   }
